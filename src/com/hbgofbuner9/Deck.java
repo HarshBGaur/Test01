a0a6a652018236d88d905f2ec775992fe7cc49cb
Harsh Bhushan Gaur <hgaur@sapient.com>
2018-11-01 11:01:34 +0000

    Collections assignment

--- src/com/hbgofbuner9/Deck.java
+++ src/com/hbgofbuner9/Deck.java
@@ -6,7 +6,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
+import java.util.Scanner;
 
 enum Suit {
     HEARTS(1), SPADES(2), DIAMONDS(3), CLUBS(4);
@@ -58,12 +58,12 @@ class Card implements Comparable<Card>{
 
     @Override
     public int compareTo(@NotNull Card card) {
-        int compareQuantity = ((Card) card).getOrder();
+        int compareOrder = ((Card) card).getOrder();
         //ascending order
-        return this.order - compareQuantity;
+        return this.order - compareOrder;
 
         //descending order
-        //return compareQuantity - this.quantity;
+        //return compareQuantity - this.compareOrder;
     }
 }
 
@@ -85,35 +85,72 @@ public class Deck {
     {
         Collections.shuffle(cards);
     }
+    static public void sortDeck()
+    {
+        Collections.sort(cards);
+    }
+    static public void displayDeck()
+    {
+        int i=1;
+        System.out.println("Deck:");
+        for(Card c : cards) {
+            System.out.format("%30s",c.print()+"\t\t");
+            if (i%5==4){System.out.println();}
+            i++;
+        }
+    }
     public static void main(String[] args) {
         //Card card = new Card(Suit.HEARTS, Rank.ACE);
         //String a = String.valueOf(card.getSuit());
         //System.out.println(a);
         //System.out.println(card.getRank());
         //System.out.println(card.print());
-
+/*
         //create Deck
         System.out.println("Creating Deck...");
         createDeck();
-        int i=1;
-        /*for(Card c : cards) {
-            System.out.println(i + " " +c.print());
-            i++;
-        }*/
+        displayDeck();
        // Shuffle Deck
         System.out.println("Shuffling:");
         shuffleDeck();
-        i=1;
-        for(Card c : cards) {
-            System.out.println(i + " " +c.print());
-            i++;
-        }
+        displayDeck();
         System.out.println("Sorting:");
-        Collections.sort(cards);
-        i=1;
-        for(Card c : cards) {
-            System.out.println(i + " " +c.print());
-            i++;
+        sortDeck();
+        displayDeck();
+*/
+        int UsrIn;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("This is a deck program");
+
+        while(true) {
+            System.out.println("\n");
+            System.out.println("Enter 1 to create deck,2 to shuffle deck,3 to sort deck,4 to display.Anything else will start this process again.");
+            UsrIn = input.nextInt();
+            System.out.println("\n");
+            int option = UsrIn;
+            switch (option) {
+                case 1:
+                    createDeck();
+                    System.out.println("\n");
+                    break;
+                case 2:
+                    shuffleDeck();
+                    System.out.println("\n");
+                    break;
+                case 3:
+                    sortDeck();
+                    System.out.println("\n");
+                    break;
+                case 4:
+                    displayDeck();
+                    System.out.println("\n");
+                    break;
+                default:
+                    System.out.println("\n");
+                    System.out.println("Repeating...");
+                    break;
+            }
         }
     }
 }
